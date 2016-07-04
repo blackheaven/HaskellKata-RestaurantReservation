e057df75700802f7807d79277140502063390933
Gautier DI FOLCO <gautier.difolco@gmail.com>
2016-07-04 18:54:21 +0200

    Factorize Validated and not Validate Reservation

--- src/ApiModel.hs
+++ src/ApiModel.hs
@@ -1,32 +1,21 @@
+{-# LANGUAGE DeriveFunctor #-}
 module ApiModel where
 
 import Data.Time (ZonedTime(..), parseTimeM, defaultTimeLocale, iso8601DateFormat)
 
-data ReservationRendition = ReservationRendition
-  { rDate :: String
-  , rName :: String
-  , rEmail :: String
-  , rQuantity :: Int }
-  deriving (Eq, Show, Read)
-
-data Reservation = Reservation
-  { date :: ZonedTime
+data ReservationContainer a = ReservationContainer
+  { date :: a
   , name :: String
   , email :: String
   , quantity :: Int }
-  deriving (Show, Read)
+  deriving (Show, Read, Functor)
+
+type ReservationRendition = ReservationContainer String
+type Reservation = ReservationContainer ZonedTime
 
 data Caravan = Caravan
   { caravanCapacity :: Int } -- Imagine that this type has more composite elements
   deriving (Eq, Show, Read)
-
-instance Eq Reservation where
-  x == y =
-    zonedTimeZone (date x) == zonedTimeZone (date y) &&
-    zonedTimeToLocalTime (date x) == zonedTimeToLocalTime (date y) &&
-    name x == name y &&
-    email x == email y &&
-    quantity x == quantity y
 
 data Error = ValidationError String | CapacityExceeded
              deriving (Show, Eq)
@@ -36,13 +25,8 @@ parseDate = parseTimeM True defaultTimeLocale (iso8601DateFormat Nothing)
 
 validateReservation :: ReservationRendition -> Either Error Reservation
 validateReservation r =
-  case parseDate (rDate r) of
-    Just d ->
-      Right Reservation
-        { date = d
-        , name = rName r
-        , email = rEmail r
-        , quantity = rQuantity r }
+  case parseDate (date r) of
+    Just d -> Right $ fmap (const d) r
     Nothing -> Left (ValidationError "Invalid date.")
 
 checkCapacity :: Int -> Int -> Reservation -> Either Error Reservation
